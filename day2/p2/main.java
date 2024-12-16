ba3f58bb40089cab241b2f702f7365cd396d8a96
William Johnson <willrayjohnson@gmail.com>
2024-12-15 21:02:17 -0600

    Day 2 puzzle 2 solution fifth attempt result: 437

--- day2/p2/main.java
+++ day2/p2/main.java
@@ -80,7 +80,7 @@ void main() {
                 dampenerHitCurNum = true;
             }
 
-            if (!isSafe && dampenerHitCurNum || dampenerHit) {
+            if (!isSafe && (dampenerHitCurNum || dampenerHit)) {
                 break;
             }
 
@@ -99,7 +99,7 @@ void main() {
                 isSafe = distance <= 3;
             }
             
-            if (!isSafe && dampenerHitCurNum || dampenerHit) {
+            if (!isSafe && (dampenerHitCurNum || dampenerHit)) {
                 break;
             }
 
